36f34ae39bd3eb340570d6fbf6df4712bc8663b2
IRTIDEATH <enderjhass@gmail.com>
2025-10-30 20:24:02 +0700

    Perubahan struktur folder dan setup cors

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,25 +1,36 @@
 import { type BetterAuthOptions, betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { admin, openAPI } from 'better-auth/plugins';
+import { admin, bearer, openAPI } from 'better-auth/plugins';
 import { PrismaService } from 'src/common/prisma.service';
+import { env } from 'src/lib/env';
 
 export const auth = betterAuth({
   database: prismaAdapter(PrismaService, {
     provider: 'postgresql',
   }),
-  plugins: [openAPI(), admin()],
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+  plugins: [openAPI(), admin(), bearer()],
   emailAndPassword: {
-    enabled: true,
-    autoSignIn: false,
+    enabled: false,
   },
   user: {
     additionalFields: {
+      role: {
+        type: "string",
+        input: false,
+      },
       premium: {
         type: 'boolean',
         input: false,
       },
     },
   },
+  trustedOrigins: [`${env.UI_URL}`],
 } satisfies BetterAuthOptions);
 
 export type Session = typeof auth.$Infer.Session;
